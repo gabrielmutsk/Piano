c3895571c33ea39581455182c605973fa2f87141
Gabriel de Sá <gabriel190706@hotmail.com>
2021-10-19 16:55:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+
+const keys = document.querySelectorAll(".key")
+
+function playNote(event) {
+  
+  let audioKeyCode = getKeyCode(event);
+
+
+  const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+
+  const cantFoundAnyKey = !key
+
+  if(cantFoundAnyKey) {
+    return;
+  }
+
+  addPlayingClass(key)
+  playAudio(audioKeyCode)
+}
+
+function addPlayingClass(key) {
+  key.classList.add('playing')
+}
+
+function getKeyCode(event) {
+  let keyCode;
+
+  const isKeyboard = event.type === "keydown"
+  if(isKeyboard) {
+    keyCode = event.keyCode
+  } else {
+    keyCode = event.target.dataset.key
+  }
+
+  return keyCode
+}
+
+function playAudio(audioKeyCode) {
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+  audio.currentTime = 0;
+  audio.play()
+}
+
+function removePlayingClass(event) {
+  event.target.classList.remove("playing")
+}
+
+function registerEvents() {
+
+  keys.forEach( function(key) {
+    key.addEventListener("click", playNote)
+    key.addEventListener("transitionend", removePlayingClass)
+  })
+
+
+  window.addEventListener("keydown", playNote)
+}
+
+window.addEventListener("load", registerEvents)
